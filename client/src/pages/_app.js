adab4c331dd62b681659ca3e03d3628841773db5
Akshay Kumar <akshaysinghr98@gmail.com>
2024-08-10 00:55:51 +0530

    backend api base url added and sigin with google

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,9 +1,19 @@
 import RootLayout from "../app/components/RootLayout";
 import axios from "axios";
 import { ThemeContextProvider } from "../app/context/ThemeContext";
+import { useEffect } from "react";
+import { checkBackendHealth } from "../app/utils/apiHealthCheck";
 
 function MyApp({ Component, pageProps }) {
-  axios.defaults.baseURL = 'http://localhost:5000';
+
+  const localUrl = "http://localhost:5000";
+  const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const isProduction = process.env.NODE_ENV === 'production';
+  axios.defaults.baseURL = isProduction ? apiUrl : localUrl;
+
+  useEffect(() => {
+    checkBackendHealth()
+  },[])
 
   return (
       <ThemeContextProvider>
